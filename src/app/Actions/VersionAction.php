c7a5eb8e2daa41ea544a5bd8f62f55b9de69ceea
Martin René Sørensen <martin@sicet7.com>
2020-09-16 16:28:19 +0200

    WIP.

--- src/app/Actions/VersionAction.php
+++ src/app/Actions/VersionAction.php
@@ -4,6 +4,7 @@ namespace App\Actions;
 
 use App\Database\Entities\TokenEntity;
 use App\Interfaces\ActionInterface;
+use App\Utility\ConfigManager;
 use Doctrine\ORM\EntityManager;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
@@ -11,26 +12,35 @@ use Slim\Interfaces\RouteCollectorInterface;
 
 class VersionAction implements ActionInterface
 {
-    /**
-     * @var EntityManager
-     */
-    private EntityManager $entityManager;
 
-    public function __construct(EntityManager $entityManager)
+    /**
+     * @var ConfigManager
+     */
+    private ConfigManager $configManager;
+
+    public function __construct(ConfigManager $configManager)
     {
-        $this->entityManager = $entityManager;
+        $this->configManager = $configManager;
     }
 
     public static function register(RouteCollectorInterface $routeCollector): void
     {
-        $routeCollector->map(['GET'], '/', static::class);
+        $routeCollector->map(['POST', 'GET'], '/', static::class);
     }
 
     public function __invoke(RequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
     {
-        $repository = $this->entityManager->getRepository(TokenEntity::class);
-        $response->getBody()
-            ->write('<pre>' . var_export($repository, true) . '</pre>');
+        $logDir = $this->configManager->get('directory.log');
+        if (!file_exists($logDir)) {
+            mkdir($logDir, 0777, true);
+        }
+        $postLogFile = $logDir . '/post-request.log';
+        if (strtolower($request->getMethod()) === 'post') {
+            file_put_contents($postLogFile, $request->getBody()->getContents() . PHP_EOL, FILE_APPEND);
+        }
+        $ipLogFile = $logDir . '/ip-request.log';
+        file_put_contents($ipLogFile, $_SERVER['REMOTE_ADDR'] . PHP_EOL, FILE_APPEND);
+        $response->getBody()->write('OK');
         return $response;
     }
 }
---
+++ src/config/directory.php
@@ -0,0 +1,7 @@
+<?php
+$root = dirname(__DIR__);
+return [
+    'root' => $root,
+    'var' => $root . '/var',
+    'log' => $root . '/var/log',
+];
